f4d8cddaaca1a290cd0b45dac9797e80859ef8e8
Ryunosuke SATO <tricknotes.rs@gmail.com>
2011-09-18 12:54:33 +0900

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TddbcPre::Application.config.secret_token = 'b82f49252702d04dde931ee6feba4c77fc2b6d69cba77b762e2363147329af1a9442fa992d92bb050b7af08b60e5eaaea7dae3250e646e3fa05ceef0da331d97'
