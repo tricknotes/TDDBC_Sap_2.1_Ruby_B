e9539d2c129f95ecdaccfc9675a94d90e5251195
Ryunosuke SATO <tricknotes.rs@gmail.com>
2011-09-19 00:42:03 +0900

    add steps for conference_rooms

---
+++ features/step_definitions/conference_rooms_steps.rb
@@ -0,0 +1,32 @@
+# encoding: utf-8
+前提 /^ユーザは "([^"]*)" としてログインしている$/ do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
+
+前提 /^システムの "([^"]*)" が表示されている$/ do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
+
+前提 /^システムに会議室情報として "([^"]*)" が登録されている$/ do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
+
+もし /^ユーザは "([^"]*)" のリンクをクリックする$/ do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
+
+ならば /^システムは "([^"]*)" ページを表示する$/ do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
+
+ならば /^ページに "([^"]*)" の会議室情報が表示されている$/ do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
+
+前提 /^システムに会議室情報が登録されていない$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+ならば /^ページに "([^"]*)" というメッセージが表示されている$/ do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
